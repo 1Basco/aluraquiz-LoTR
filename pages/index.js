2001c72ac93f92e2fbdbcc7bce3f92b08e1ca99c
1Basco <lsos13@gmail.com>
2021-01-28 12:35:33 -0300

    codigo ao final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Widget from '../src/components/Widget'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>League of Legends</h1>
+          </Widget.Header>
+          <Widget.Content>
+           <p>MEME BUZINA</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>League of Legends</h1>
+          </Widget.Header>
+          <Widget.Content>
+           <p>MEME BUZINA</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/1Basco"/>
+    </QuizBackground>
+
+  );
 }
